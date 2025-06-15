29cbd1bc2289644103de1524c61878c8f0e0f0eb
s0216730 <s0216730@ad.ua.ac.be>
2025-06-15 21:20:21 +0200

    feat<Transactions>: Created basic table

--- ftw_frontend/src/pages/transactions.tsx
+++ ftw_frontend/src/pages/transactions.tsx
@@ -1,8 +1,36 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
 export default function TransactionsPage() {
   return (
     <div>
-      <h1>Transactions</h1>
-      <p>This is the transactions page.</p>
+      <h1 className='text-xl font-bold underline'>Transactions</h1>
+      <Table>
+        <TableHeader>
+          <TableRow className='text-left'>
+            <TableHead>ID</TableHead>
+            <TableHead>Category</TableHead>
+            <TableHead>Description</TableHead>
+            <TableHead>Amount</TableHead>
+            <TableHead>Date</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow className='text-left'>
+            <TableCell>1</TableCell>
+            <TableCell>Test</TableCell>
+            <TableCell>Something longer. Lorem ipsum ...</TableCell>
+            <TableCell>€150</TableCell>
+            <TableCell>25/01/2004 12:00</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </div>
   );
 }
